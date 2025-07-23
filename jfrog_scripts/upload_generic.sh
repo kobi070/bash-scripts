e66544f24ba45ac72314ba6142c0102e17180a0e
KobiK <kobi@rafael.co.il>
2025-07-23 13:52:34 +0300

    [main] fixing script for upload for jfrog - files: jfrog_scripts/upload_generic.sh

--- jfrog_scripts/upload_generic.sh
+++ jfrog_scripts/upload_generic.sh
@@ -2,8 +2,20 @@
 set -e
 
 # This script uploads a file to JFrog Artifactory using the generic upload API.
-# Usage: ./upload_generic.sh <file_path> <target_path>
-curl -v -H X-JFrog-Art-Api:$JFROG_API_KEY \
-     -H "Content-Type: application/octet-stream" \
-     -T "$1" \
-     "$JFROG_URL/api/storage/$JFROG_REPO/"
+# Usage: ./upload_generic.sh <local_file_path> <target_path_in_repo>
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <local_file_path> <target_path_in_repo>"
+  exit 1
+fi
+
+LOCAL_FILE="$1"
+TARGET_PATH="$2"
+
+curl -v \
+  -H "X-JFrog-Art-Api: $JFROG_API_KEY" \
+  -H "Content-Type: application/octet-stream" \
+  -T "$LOCAL_FILE" \
+  "$JFROG_URL/$JFROG_REPO/$TARGET_PATH"
+
+echo "File uploaded successfully to $JFROG_URL/$JFROG_REPO/$TARGET_PATH"
